6eebf9bd3e64df79a04fc551a4255d35e41f03d6
YuliyaKis85 <114609759+YuliyaKis85@users.noreply.github.com>
2022-10-02 12:11:17 -0400

    add car type

--- Car.java
+++ Car.java
@@ -1,6 +1,10 @@
 public class Car {
-    String type;
-    String model;
-    String color;
-    String window;
+    public static void main (String[] args) {
+        String type = "BMW";
+        String model = "X6";
+        String color= "Black";
+        int window= 5;
+        System.out.println(type +" "+ model+" " + color +" "+ window);
+    }
+
 }
